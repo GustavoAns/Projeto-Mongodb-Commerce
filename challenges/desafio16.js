590f5a3f2a070fb1e280d1d170a318724fc16db0
GustavoAns <gugasato9@live.com>
2022-04-05 18:57:53 -0300

    Desafio 16

--- challenges/desafio16.js
+++ challenges/desafio16.js
@@ -8,15 +8,3 @@ db.produtos.find(
   {ultimaModificacao: { $exists: true } },
   { nome: 1, _id: 0 }
 );
-
-
-// db.produtos.updateMany(
-//   {nome: "Big Mac"},
-//   { $set: {
-//     $currentDate: { ultimaModificacao: { lastModified: true }}
-//   }}
-// );
-// db.produtos.find(
-//   {ultimaModificacao: { $exists: true } },
-//   { nome: 1, _id: 0 }
-// );
